99531839c8ea3a7602cb286517f7a70e53cf5b0c
Lukas Weber <lukmweber@gmail.com>
2015-07-30 19:43:13 +0200

    fix error in checking whether inputs are lists

--- R/fitRegModel.R
+++ R/fitRegModel.R
@@ -138,9 +138,15 @@ fitRegModel <- function(X_genes = NULL, Y_genes, weights_genes = NULL,
                         alpha = 1, lambda_choice = c("lambda.min", "lambda.1se"), 
                         return_fitted = FALSE, ncores = 1, ...) {
   
-  if (!is.list(X_genes)) X_genes <- list(X_genes = X_genes)
-  if (!is.list(Y_genes)) Y_genes <- list(Y_genes = Y_genes)
-  if (!is.list(weights_genes)) weights_genes <- list(weights_genes = weights_genes)
+  if (!is.null(X_genes) && !is.list(X_genes)) {
+    X_genes <- list(X_genes = X_genes)
+  }
+  if (!is.list(Y_genes)) {
+    Y_genes <- list(Y_genes = Y_genes)
+  }
+  if (!is.null(weights_genes) && !is.list(weights_genes)) {
+    weights_genes <- list(weights_genes = weights_genes)
+  }
   
   FUN <- function(i) {
     fitRegModelSingle(X = X_genes[[i]], Y = Y_genes[[i]], weights = weights_genes[[i]], 
