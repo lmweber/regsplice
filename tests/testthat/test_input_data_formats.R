c03517911816fc693d682b2bc0d6afd9ab4a7c6b
Lukas M Weber <lukmweber@gmail.com>
2016-10-27 10:31:10 +0000

    unit test for SummarizedExperiment data inputs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/regsplice@123102 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_input_data_formats.R
+++ tests/testthat/test_input_data_formats.R
@@ -12,7 +12,16 @@ test_that("counts matrix can be input as matrix or data frame", {
   rs_data_matrix <- RegspliceData(counts,    gene_IDs, n_exons, condition)
   rs_data_df     <- RegspliceData(counts_df, gene_IDs, n_exons, condition)
   
+  se <- SummarizedExperiment::SummarizedExperiment(
+    assays = S4Vectors::SimpleList(counts = counts), 
+    rowData = S4Vectors::DataFrame(gene_IDs = rep(gene_IDs, n_exons)), 
+    colData = S4Vectors::DataFrame(condition))
+  
+  rs_se <- RegspliceData(se)
+  
   expect_equivalent(rs_data_matrix, rs_data_df)
+  expect_equivalent(rs_data_matrix, rs_se)
+  expect_equivalent(rs_data_df, rs_se)
 })
 
 
